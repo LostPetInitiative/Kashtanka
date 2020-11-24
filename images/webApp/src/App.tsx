4ac4f804bb7396972875d3353ce5386a90fc44ee
Lucy Mukhametdinova <v-lumukh@microsoft.com>
2020-11-24 19:49:44 +0300

    edited animal card

--- images/webApp/src/App.tsx
+++ images/webApp/src/App.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import './DataModel'
 import './App.css';
 import logo from './catndog.png';
-import {GetCard} from "./CardStorage.js";
+import {GetCard, GetPetCard} from "./CardStorage";
 import AnimalCard from "./AnimalCard";
+import { AnimalCard as AnimalCardDM} from "./DataModel";
 
 
 /*------------------------------------------------------------------------------------------*/
@@ -11,14 +12,22 @@ import AnimalCard from "./AnimalCard";
 function App() {
   var paramsString = window.location.search;
   var searchParams = new URLSearchParams(paramsString);
-  var animal = searchParams.has("animal") ? (searchParams.get("animal")) : null;
+
+  var animalType = searchParams.has("animal") ? searchParams.get("animal") : "none";
+  var animal : string = !animalType ? "none" : animalType;  
+
+  const cardID = searchParams.has("cardID") ? searchParams.get("cardID") : "none";
+  const id : string = !cardID ? "none" : cardID;
+  const cardDomain = searchParams.has("cardDomain") ? searchParams.get("cardDomain") : "none";
+  const domain : string = !cardDomain ? "none" : cardDomain;
+
+  //const card : AnimalCardDM = animal != "" ? GetCard(animal)
   
-  var card = GetCard(animal);
+  const card = GetPetCard(id, domain);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <AnimalCard card={card}/>
       </header>
     </div>
