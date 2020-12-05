ed4234ccf200c73e922a0cb85bc8455ddc3465c8
Lucy Mukhametdinova <v-lumukh@microsoft.com>
2020-12-05 15:26:27 +0300

    added svg icons for cats and dogs for both genders

--- images/webApp/src/AnimalCard.tsx
+++ images/webApp/src/AnimalCard.tsx
@@ -8,8 +8,13 @@ function AnimalCard(props : {card: DataModel.AnimalCard}) {
     const card = props.card;
 
     function animalType(animalType: DataModel.Animal, animalGender: DataModel.Sex) {
-        if(animalType == DataModel.Animal.Dog)
-            return <img src="./img/dog/dogFaceWhite.png" className="animalTypeImg"/>;
+        if(animalType == DataModel.Animal.Dog) {
+            if(animalGender == DataModel.Sex.Female)
+                return <img src="./img/dog/female.svg" className="animalTypeImg"/>;
+            else if(animalGender == DataModel.Sex.Male)
+                return <img src="./img/dog/male2.svg" className="animalTypeImg"/>;
+            return  <img src="./img/dog/dog.svg" className="animalTypeImg"/>;
+        }
         else if(animalType == DataModel.Animal.Cat) {
             if(animalGender == DataModel.Sex.Female)
                 return <img src="./img/cat/female.svg" className="animalTypeImg"/>;
