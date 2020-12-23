8460919f050be0bd1ca04cba4de231f0c1fc1909
Dmitry A. Grechka <dmitry@grechka.family>
2020-12-24 01:34:16 +0300

    transitioning to separate kafka-job-queue wheel package v0.1.0; duplicates remover, pet detector and features extractor are updated

--- images/processedCardsPersister/code/npSerialization.py
+++ images/processedCardsPersister/code/npSerialization.py
@@ -5,7 +5,7 @@ def npArrayToBase64str(npArray):
     npArrayBytes = npArray.tobytes()
     return base64.encodebytes(npArrayBytes).decode("utf-8").replace("\n","")
 
-def Base64strToNpArray(b64Str:str):
+def base64strToNpArray(b64Str:str):
     bytesObj = base64.decodebytes(b64Str.encode("utf-8"))
     npArray = np.frombuffer(bytesObj, dtype=np.float32)
     return npArray
