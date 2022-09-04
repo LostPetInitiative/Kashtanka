8182c52b7c091fb8ea6b0a9f07aa554f6fb6f74f
Dmitry A. Grechka <dmitry@grechka.family>
2022-09-04 17:13:31 +0000

    KafkaRestAPI: bump quart to 0.18.*; increasing permitted request size. resolves #24

--- images/kafkaRestAPI/code/api.py
+++ images/kafkaRestAPI/code/api.py
@@ -6,6 +6,7 @@ import os
 kafkaUrl = os.environ['KAFKA_URL']
 
 app = Quart(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024*1024*256 # 256Mb
 
 kafkaTopicsCache = dict()
 
@@ -23,8 +24,8 @@ async def post_message(topic_name):
         kafkaTopicsCache[topic_name] = kafkajobs.jobqueue.JobQueueProducer(kafkaUrl, topic_name, "KafkaRestAPI")
         print(f"Created producer for topic {topic_name}")
     
-    producer = kafkaTopicsCache[topic_name]
-    data_json = await request.get_json()
+    producer = kafkaTopicsCache[topic_name]    
+    data_json = await request.get_json()    
     print("sending #{0} to topic {1}".format(counter, topic_name))
     with lock:
       producer.Enqueue(str(counter), data_json)
@@ -33,6 +34,7 @@ async def post_message(topic_name):
     counter += 1
     return 'Posted', 201
   except Exception as ex1:
+    print(f"Exception: {ex1}. Replying 500")
     return f'Error - {ex1}',500
 
 if __name__ == "__main__":
